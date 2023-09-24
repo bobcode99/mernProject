f4381d6b6531bfee9eb8391958a9635a67479a2f
yb <lojobs38@gmail.com>
2023-09-24 20:14:14 +0800

    format: install eslint, prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,37 +3,37 @@ import { createServer } from "miragejs";
 
 // Create a MirageJS server instance
 const server = createServer({
-  routes() {
-    this.namespace = "api";
+    routes() {
+        this.namespace = "api";
 
-    this.get("/users", () => {
-      return {
-        users: [
-          { id: 1, name: "Bob" },
-          { id: 2, name: "Jiali" },
-        ],
-      };
-    });
-  },
+        this.get("/users", () => {
+            return {
+                users: [
+                    { id: 1, name: "Bob" },
+                    { id: 2, name: "Jiali" },
+                ],
+            };
+        });
+    },
 });
 
 export default function App() {
-  let [users, setUsers] = useState<{ id: number; name: string }[]>([]);
+    let [users, setUsers] = useState<{ id: number; name: string }[]>([]);
 
-  useEffect(() => {
-    fetch("/api/users")
-      .then((res) => res.json())
-      .then((json) => {
-        setUsers(json.users);
-        console.log(json);
-      });
-  }, []);
+    useEffect(() => {
+        fetch("/api/users")
+            .then((res) => res.json())
+            .then((json) => {
+                setUsers(json.users);
+                console.log(json);
+            });
+    }, []);
 
-  return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
-  );
+    return (
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+    );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import "./index.css";
-import Counter from "./Counter.tsx";
+// import Counter from "./Counter.tsx";
+import FilterableProductTable, { PRODUCTS } from "./FilterableProductTable.tsx";
+import FilterableLimitTable from "./FilterableLimitTable.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Counter />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <App /> */}
+        {/* <Counter /> */}
+        {/* <FilterableProductTable products={PRODUCTS} /> */}
+        <FilterableLimitTable />
+    </React.StrictMode>
 );
