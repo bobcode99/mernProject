156113217e38e4ced8dbd3c8403c2f307ce83402
yb <lojobs38@gmail.com>
2023-10-03 22:37:03 +0800

    install testcontainer

--- backend/src/tests/test1.test.ts
+++ backend/src/tests/test1.test.ts
@@ -1,5 +1,34 @@
-import { describe, expect, it, test } from "vitest";
+import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
+import {
+    startedMongoTestContainerOf,
+    StartedMongoTestContainer,
+} from "testcontainers-mongoose";
+import { serverOf, serverStart } from "../server";
+import { AppConfig } from "../types/appConfig";
 
-it("should right", () => {
-    expect(1 + 2).toBe(3);
+describe("mongo test container test", () => {
+    let mongoTestContainer: StartedMongoTestContainer;
+    const server = serverOf();
+
+    beforeAll(async () => {
+        mongoTestContainer = await startedMongoTestContainerOf("mongo");
+
+        const appConfigForTest: AppConfig = {
+            FASTIFY_HOST: "localhost",
+            FASTIFY_PORT: 8889,
+            MONGO_CONNECTION_STRING: mongoTestContainer.getUri(),
+        };
+
+        await serverStart(server)(appConfigForTest);
+    });
+
+    afterAll(async () => {
+        await mongoTestContainer.closeDatabase();
+    });
+
+    afterEach(async () => {
+        await mongoTestContainer.clearDatabase();
+    });
+
+    it("Should be 200 status code", async () => {});
 });
